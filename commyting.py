58f4acedd024b6cfa9e11d628bb2add50581b7a9
CL264370 <CL264370@163.com>
2025-10-30 00:20:41 +0800

    通勤方式

---
+++ commyting.py
@@ -0,0 +1,6 @@
+commyting = ['ship','airplane','motorcycle','bicycle','car']
+print('I would like to own a '+commyting[0])
+print('I would like to own a '+commyting[1])
+print('I would like to own a '+commyting[2])
+print('I would like to own a '+commyting[3])
+print('I would like to own a '+commyting[4])
