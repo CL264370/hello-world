23f61982a011879b9d45e768b29e496033901ea1
CL264370 <CL264370@163.com>
2025-11-01 23:59:25 +0800

    ABC

---
+++ pizzas.py
@@ -0,0 +1,15 @@
+pizzas = ['Neapolitan pizza','Hawaiian pizza','Fruit pizza','Cheese pizza']
+for pizza in pizzas:
+    print("I like " + pizza)
+print('I really love pizza')
+animals = ['cat','dog','tiger']
+for animal in animals:
+    print('A ' + animal + 'has four legs')
+print('Any of these anmials would make a great pet')
+for value in range(1,21):
+    print(value)
+squares = list(range(3,31,3))
+for list in squares:
+    print(list)
+cubes = [value**3 for value in range(1,11)]
+print(cubes)
